ec77d44c4728f8500b0da44ab0d2be9549195235
Diogo Bastos <diogo.bastos@hmhco.com>
2021-04-19 13:23:53 -0400

    Menu

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,20 +1,66 @@
+import { AppBar, CssBaseline, Divider, Drawer, List, ListItem, ListItemText, makeStyles, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
-import { Agenda } from './Agenda.jsx';
-import { AoVivo } from './AoVivo.jsx';
-import { Comunidade } from './Comunidade.jsx';
-import { Contato } from './Contato.jsx';
-import { Contribua } from './Contribua.jsx';
-import { Missao } from './Missao.jsx';
-import { Pastoral } from './Pastoral.jsx';
 
-export const App = () => (
-  <div>
-    <Agenda/>
-    <AoVivo/>
-    <Comunidade/>
-    <Contato/>
-    <Contribua/>
-    <Missao/>
-    <Pastoral/>
-  </div>
-);
+
+export const App = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar className={classes.toolbarProps}>
+          <Typography variant="h6" noWrap>
+            Ip Mosaico
+          </Typography>
+        </Toolbar>
+      </AppBar>
+       <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+          {['Pastoral', 'Agenda', 'Ao Vivo', 'Comunidade', 'Contato', 'Contribua', 'Missão'].map((text) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </div>
+  );
+};
+
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    color: 'white',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  toolbarProps: {
+    backgroundColor: 'white',
+    color: 'black'
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+}));
+
