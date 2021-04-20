97d1edce3705591f00f5b6c5e16f5a3f272473f5
Diogo Bastos <diogo.bastos@hmhco.com>
2021-04-19 22:19:17 -0400

    Pastoral list table

--- imports/ui/PastoralList.jsx
+++ imports/ui/PastoralList.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 import { PastoralCollection } from '../collections/pastoral';
+import { makeStyles, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
 
 export const PastoralList = () => {
-  const pastoral = useTracker(() => {
-    return PastoralCollection.find().fetch();
-  });
+    const classes = useStyles();
+    const pastoral = useTracker(() => {
+        return PastoralCollection.find().fetch();
+    });
 
-  const renderPastorais = () => {
-    return pastoral.map((past) => {
-        const { titulo, autor} = past;
-        return (
-            <tr key={titulo}>
-                <td>{titulo}</td>
-                <td>{autor}</td>
-            </tr>
-        )
-    })
-  };
-
-  return (
-   <table className="table">
-       <thead>
-           <tr>
-               <th>Titulo</th>
-               <th>Autor</th>
-           </tr>
-       </thead>
-       <tbody>
-           {renderPastorais()}
-       </tbody>
-   </table>
-  );
+    return (
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Titulo</TableCell>
+                        <TableCell align="right">Autor</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {
+                        pastoral.map((past) => (
+                            <TableRow key={past.titulo}>
+                                <TableCell component="th" scope="row">{past.titulo}</TableCell>
+                                <TableCell align="right">{past.autor}</TableCell>
+                            </TableRow>
+                        ))
+                    }
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 };
