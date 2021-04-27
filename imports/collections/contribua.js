e58c3adf0d3784019bad2772909dc105d95e5549
Diogo Bastos <diogo.bastos@hmhco.com>
2021-04-26 23:20:14 -0400

    Contribua form

--- imports/collections/contribua.js
+++ imports/collections/contribua.js
@@ -1,3 +1,44 @@
 import { Mongo } from 'meteor/mongo';
+import { Meteor } from 'meteor/meteor';
+import { check, Match } from 'meteor/check';
+
+
+Meteor.methods({
+  'contribua.update': (id, nomeBanco, banco, agencia, cc, operacao, igreja, cnpj) => {
+    check(
+      nomeBanco,
+      Match.Where((nomeBanco) => nomeBanco.length),
+    );
+    check(
+      banco,
+      Match.Where((banco) => banco.length),
+    );
+    check(
+      agencia,
+      Match.Where((agencia) => agencia.length),
+    );
+    check(
+      cc,
+      Match.Where((cc) => cc.length),
+    );
+    check(
+      operacao,
+      Match.Where((operacao) => operacao.length),
+    );
+    check(
+      igreja,
+      Match.Where((igreja) => igreja.length),
+    );
+    check(
+      igreja,
+      Match.Where((igreja) => igreja.length),
+    );
+
+    ContribuaCollection.update(
+      { _id: id },
+      { nomeBanco, banco, agencia, cc, operacao, igreja, cnpj, updatedAt: new Date() },
+    );
+  },
+});
 
 export const ContribuaCollection = new Mongo.Collection('contribua');
