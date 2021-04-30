42fef895bb0b56b16fdb0a7850fb5bd19a8f997f
Diogo Bastos <diogo.bastos@hmhco.com>
2021-04-29 23:46:39 -0400

    bem vindo pastoral

--- server/pastoral.js
+++ server/pastoral.js
@@ -11,25 +11,16 @@ function insertPastoral({ titulo, autor, descricao }) {
 
 export function inserirPastoral() {
   insertPastoral({
-    titulo: 'A melhor opção de investimento',
-    autor: 'Marcos E. Fink',
-    descricao: `
-      Às vezes, as pessoas me perguntam qual é a melhor opção de investimento. A minha resposta não é a que elas esperam, mas é absolutamente verdadeira e está baseada nos seguintes princípios:
-    
-      "O Reino dos céus é como um tesouro escondido num campo. Certo homem, tendo-o encontrado, escondeu-o de novo e, então, cheio de alegria, foi, vendeu tudo o que tinha e comprou aquele campo" Mt 13:44.
-    
-      "Não acumulem para vocês tesouros na terra, onde a traça e a ferrugem destroem, e onde os ladrões arrombam e furtam. as acumulem para vocês tesouros nos céus, onde a traça e a ferrugem não destroem, e onde os ladrões não arrombam nem furtam" Mt 6:19-20.
-    
-      Os rendimentos desse tipo de investimento são imensuráveis e eternamente garantidos. Mas não é qualquer pessoa que pode investir no "banco" do Reino dos céus. Para investir nesse "banco" também é necessário "abrir uma conta". E para "abrir uma conta", é fundamental primeiramente ter cidadania do Reino.
-    
-      Você já é cidadão do Reino dos céus? Você já tem "uma conta no banco" do Reino? Você conhece o "gerente" desse "banco"? E o "banqueiro"? Você já está apto a investir no Reino?
-    
-      Se a sua resposta a essas perguntas é 'não', você precisa conhecer o Reino urgentemente. Por onde começar? Pegue uma Bíblia e estude o Novo Testamento. Comece lendo o evangelho de Mateus ou o evangelho de João. 
-      
-      Continue estudando a Bíblia e aprenda tudo o que você precisa saber sobre o Reino, primordialmente sobre o Rei desse Reino. Assim, terá o privilégio de investir no Reino desde já, cheio de alegria.
-    
-      Se a sua resposta às perguntas acima é 'sim', deixe-me perguntar: Os investimentos que você tem feito têm valor no Reino? Como está o "saldo" da sua "conta de investimentos" do Reino? Continue investindo pesado no Reino, cheio de alegria.
-      
-      Que o Rei do Reino te conceda oportunidades de investir no que verdadeiramente vale à pena investir.`,
+    titulo: 'BEM VINDO!',
+    autor: 'Felipe Lobo Andrade',
+    descricao: `É com grande alegria que damos boas vindas ao Aplicativo da Igreja Presbiteriana Mosaico. Somos muito gratos a Deus por mais uma ferramenta na propagação do evangelho e na edificação da igreja de Jesus.
+
+      Essa é a primeira versão do nosso aplicativo, sabemos que aos poucos vamos melhorando e aumentando as ferramentas por aqui cada vez mais. O intuito do nosso aplicativo não é outro senão usarmos de todo artifício em nossas mãos para que o evangelho seja conhecido, para que a comunidade seja edificada e para que os irmãos estejam sempre em comunhão.
+
+      Através dessa plataforma você terá acesso à mensagens pastorais, comunicados importantes, links rápidos para YouTube, Facebook, Instagram, Whatsapp, etc. Você também pode entrar em contato conosco e diretamente com nosso pastor!
+
+      A Palavra de Deus em Romanos 14.19 diz: “Por isso, esforcemo-nos em promover tudo quanto conduz à paz e à edificação mútua.” É justamente nesse propósito que nosso aplicativo foi criado, para ser canal de edificação, de paz, de comunhão e de evangelização.
+
+      Que Deus te abençoe!`,
   });
 }
